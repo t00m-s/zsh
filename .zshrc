870195735dbde3ada3da6461595d9101ae1a2c48
Tommaso Soncin <soncintommaso@gmail.com>
2025-02-05 10:35:17 +0100

    feat: all zsh files in repo
    I still don't know if committing antigen.zsh file makes sense but right
    now I'll do it this way.

---
+++ .zshrc
@@ -0,0 +1,3 @@
+source $HOME/.config/zsh/plugins.zsh
+source $HOME/.config/zsh/aliases.zsh
+eval "$(zoxide init zsh)"
--- aliases.zsh
+++ aliases.zsh
@@ -10,3 +10,12 @@ v() {
 }
 alias lg=lazygit
 alias ldo=lazydocker
+alias ls='eza $eza_params'
+alias l='eza --git-ignore $eza_params'
+alias ll='eza --all --header --long $eza_params'
+alias llm='eza --all --header --long --sort=modified $eza_params'
+alias la='eza -lbhHigUmuSa'
+alias lx='eza -lbhHigUmuSa@'
+alias lt='eza --tree $eza_params'
+alias tree='eza --tree $eza_params'
+alias cd="z"
